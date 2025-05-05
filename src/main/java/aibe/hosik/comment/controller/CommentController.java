b377d94d24e912b9f39cf0a1ee0ad90c4cda79be
s0___0k <61587396+s0ooo0k@users.noreply.github.com>
2025-05-06 05:45:43 +0900

    docs : CommentController Javadoc 제거
    - 코드 변경 없이 CommentController의 주석은 Swagger Operation으로 대체

--- src/main/java/aibe/hosik/comment/controller/CommentController.java
+++ src/main/java/aibe/hosik/comment/controller/CommentController.java
@@ -24,13 +24,6 @@ import java.util.List;
 public class CommentController {
   private final CommentService commentService;
 
-  /**
-   * 댓글 또는 대댓글을 등록하는 메서드입니다.
-   *
-   * @param dto 댓글 등록 요청 데이터를 담는 객체
-   * @param user 현재 인증된 사용자의 정보를 담고 있는 객체
-   * @return HTTP 상태가 CREATED인 ResponseEntity 객체
-   */
   @Operation(summary="댓글 등록", description="댓글 및 대댓글을 등록합니다.")
   @PostMapping
   public ResponseEntity<?> createComment(@RequestBody CommentRequestDTO dto, @AuthenticationPrincipal User user) {
@@ -38,12 +31,6 @@ public class CommentController {
     return ResponseEntity.status(HttpStatus.CREATED).build();
   }
 
-  /**
-   * 특정 게시글의 모든 댓글 및 대댓글을 조회하는 메서드입니다.
-   *
-   * @param postId 댓글을 조회할 게시글의 ID
-   * @return 댓글과 대댓글 정보를 담은 CommentResponseDTO 객체의 리스트를 포함한 ResponseEntity
-   */
   @Operation(summary="댓글 조회", description=" 모든 댓글 및 대댓글을 조회합니다.")
   @GetMapping
   public ResponseEntity<List<CommentResponseDTO>> getComments(@RequestParam Long postId){
@@ -51,13 +38,6 @@ public class CommentController {
     return ResponseEntity.ok(comments);
   }
 
-  /**
-   * 댓글을 삭제하는 메서드입니다.
-   *
-   * @param commentId 삭제하려는 댓글의 ID
-   * @param user 현재 인증된 사용자의 정보를 담고 있는 객체
-   * @return HTTP 상태가 NO_CONTENT인 ResponseEntity 객체
-   */
   @Operation(summary="댓글 삭제", description="댓글을 삭제합니다.")
   @DeleteMapping("/{commentId}")
   public ResponseEntity<?> deleteComment(@PathVariable Long commentId, @AuthenticationPrincipal User user) {
