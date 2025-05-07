1b29050c0f19f38b72ba0d42fce9e3f8ab7bb189
s0___0k <61587396+s0ooo0k@users.noreply.github.com>
2025-05-07 16:08:43 +0900

    Revert "refactor : JWT 통합 위한 테스트용 파일 삭제"
    This reverts commit 03791ce81e68e63a84daf6d9b6271e8a5a42390d.

---
+++ src/main/java/aibe/hosik/post/controller/PostMockController.java
@@ -0,0 +1,60 @@
+package aibe.hosik.post.controller;
+
+import aibe.hosik.post.dto.PostDetailDTO;
+import aibe.hosik.post.dto.PostPatchDTO;
+import aibe.hosik.post.dto.PostRequestDTO;
+import aibe.hosik.post.dto.PostResponseDTO;
+import aibe.hosik.post.entity.Post;
+import aibe.hosik.post.facade.PostFacade;
+import aibe.hosik.post.service.PostService;
+import aibe.hosik.skill.repository.PostSkillRepository;
+import aibe.hosik.user.User;
+import aibe.hosik.user.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Tag(name = "[Mock] 모집글 테스트 API", description = "Swagger 테스트용 인증 없는 모집글 CRUD API")
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/posts")
+public class PostMockController {
+
+    private final PostFacade postFacade;
+    private final UserRepository userRepository;
+
+    @Operation(summary = "[로컬 TEST용] 모집글 등록 테스트", description = "[TEST] 모집글을 등록합니다.")
+    @PostMapping("/mock")
+    public ResponseEntity<PostResponseDTO> createPostForSwagger(@RequestBody PostRequestDTO dto) {
+        User mockUser = userRepository.findById(1L).orElseThrow();
+        PostResponseDTO responseDTO = postFacade.createPost(dto, mockUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+    }
+
+    @Operation(summary = "[로컬 TEST용] 모집글 수정 테스트", description = "[TEST] 모집글을 수정합니다.")
+    @PatchMapping("/mock/{postId}")
+    public ResponseEntity<PostResponseDTO> updatePostForSwagger(@PathVariable Long postId,
+                                                                @RequestBody PostPatchDTO dto) {
+        User mockUser = userRepository.findById(1L).orElseThrow();
+        PostResponseDTO responseDTO = postFacade.updatePost(postId, dto, mockUser);
+        return ResponseEntity.ok(responseDTO);
+    }
+
+    @Operation(summary = "[로컬 TEST용] 모집글 삭제 테스트", description = "[TEST] 테스트용 유저로 모집글을 삭제합니다.")
+    @DeleteMapping("/mock/{postId}")
+    public ResponseEntity<?> deletePostForSwagger(@PathVariable Long postId) {
+        User mockUser = userRepository.findById(1L).orElseThrow();
+        postFacade.deletePost(postId, mockUser);
+        return ResponseEntity.noContent().build();
+    }
+}
